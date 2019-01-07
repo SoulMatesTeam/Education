2bd187ac76716e046f38ff358456baac34a50b68
smart_freeman <alex.wolf.petrov@gmail.com>
2019-01-07 23:01:00 +0200

    Added project for matrices

---
+++ WorkWithMatrices/MatrixLibrary/IntMatrix.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatrixLibrary
+{
+    public class IntMatrix
+    {
+        private int _rows;
+        private int _columns;
+
+        private int[,] _matrix;
+
+        public int Rows => _rows;
+        public int Columns => _columns;
+
+        public IntMatrix(int rows, int columns)
+        {
+            _matrix = new int[rows, columns];
+            _rows = rows;
+            _columns = columns;
+        }
+
+        public void SetElement(int rowNumber, int columnNumber, int val)
+        {
+            _matrix[rowNumber, columnNumber] = val;
+        }
+
+        public int GetElement(int rowNumber, int columnNumber)
+        {
+            return _matrix[rowNumber, columnNumber];
+        }
+    }
+}
---
+++ WorkWithMatrices/WorkWithMatrices/Program.cs
@@ -0,0 +1,32 @@
+﻿using MatrixLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorkWithMatrices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntMatrix matrix = new IntMatrix(5,5);
+            FillWithRandomElements(matrix);
+
+
+            Console.ReadLine();
+        }
+
+        static void FillWithRandomElements(IntMatrix matrix)
+        {
+            Random r = new Random();
+            for (int i = 0; i < matrix.Rows; i++)
+            {
+                for (int j = 0; j < matrix.Columns; j++)
+                {
+                    matrix.SetElement(i, j, r.Next(0, 11));
+                }
+            }
+        }
+    }
+}
